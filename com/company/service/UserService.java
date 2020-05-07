d5167955c5f50d8819c1f9e9f4d75b3312a34860
RegieZ <14253801@life.hkbu.edu.hk>
2020-05-08 00:33:17 +0800

    Update UserService.java

--- com/company/service/UserService.java
+++ com/company/service/UserService.java
@@ -8,15 +8,28 @@ import java.util.List;
 public class UserService {
 
     UserDao userDao = new UserDao();
-    
+
     public List<User> findAll() {
         // 调用dao查询
         //  List<User> list = userDao.findAll();
         return userDao.findAll();
     }
-    
-    public void add(User user){
+
+    public void add(User user) {
         // 调用dao添加
         userDao.add(user);
+    }
+
+    public void delete(String id) {
+        userDao.delete(id);
+    }
+
+    public User findById(String id){
+        // 调用dao，根据id查询
+        return userDao.findById(id);
+    }
+
+    public void update(User newUser){
+        userDao.update(newUser);
     }
 }
